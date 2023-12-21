9c4c4593f6a823a356f8f81cd7fcdfca2193d4a6
anafs8075 <anafs8075@gmail.com>
2023-12-21 17:52:34 +0900

    삭제 기능 구현, 없는 명언일 경우에 대한 처리

--- src/com/ws/wiseSaying/controller/WiseSayingController.java
+++ src/com/ws/wiseSaying/controller/WiseSayingController.java
@@ -1,11 +1,9 @@
 package com.ws.wiseSaying.controller;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.ws.Container;
 import com.ws.Rq;
 import com.ws.wiseSaying.entity.WiseSaying;
-
 public class WiseSayingController {
 	private int lastId;
 	private List<WiseSaying> wiseSayings;
@@ -28,17 +26,14 @@ public class WiseSayingController {
 		if (wiseSayings.size() == 0) {
 			System.out.println("등록 된 명언이 없어");
 		} else {
-			System.out.println("번호  /  작가   /  명언  ");
+			System.out.println("번호  /  작가  /  명언  ");
 			System.out.println("=".repeat(30));
 			for (int i = wiseSayings.size() - 1; i >= 0; i--) {
 				WiseSaying ws = wiseSayings.get(i);
 				System.out.printf("%d  /  %s  /  %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
 			}
 		}
-
 	}
-
-
 	public void remove(Rq rq) {
 		int id = rq.getIntParam("id", -1);
 		if (id == -1) {
@@ -46,8 +41,27 @@ public class WiseSayingController {
 			return;
 		}
 
+		WiseSaying wiseSaying = findById(id);
+
+		if (wiseSaying == null) {
+			System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+			return;
+		}
+
+		wiseSayings.remove(wiseSaying);
+
 		System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
 
+	}
+
+	private WiseSaying findById(int id) {
+		for (WiseSaying wiseSaying : wiseSayings) {
+			if (wiseSaying.getId() == id) {
+				return wiseSaying;
+			}
+		}
+
+		return null;
 	}
 
 }
