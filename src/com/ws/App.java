a30a37e722143494cb94631cd736f8073f761129
anafs8075 <anafs8075@gmail.com>
2023-12-21 14:39:33 +0900

    명령어 파싱

---
+++ src/com/ws/App.java
@@ -0,0 +1,57 @@
+package com.ws;
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ws.system.controller.SystemController;
+import com.ws.wiseSaying.controller.WiseSayingController;
+public class App {
+	public App() {
+	}
+	public void run() {
+		System.out.println("== 명언 앱 실행 ==");
+		SystemController systemController = new SystemController();
+		WiseSayingController wiseSayingController = new WiseSayingController();
+		while (true) {
+			System.out.print("명령어 ) ");
+			String cmd = Container.getScanner().nextLine().trim();
+			if (cmd.equals("종료")) {
+				systemController.exit();
+				break;
+			} else if (cmd.equals("등록")) {
+				wiseSayingController.write();
+			} else if (cmd.equals("목록")) {
+				wiseSayingController.list();
+			} else if (cmd.startsWith("삭제")) {
+				// parsing
+
+				String[] cmdBits = cmd.split("\\?", 2);
+
+				String actionCode = cmdBits[0];
+				Map<String, String> params = new HashMap<>();
+
+				String[] paramBits = cmdBits[1].split("&");
+
+				for (String paramStr : paramBits) {
+					String[] paramStrBits = paramStr.split("=", 2);
+					String key = paramStrBits[0];
+					String value = paramStrBits[1];
+					System.out.println("key : " + key);
+					System.out.println("value : " + value);
+					params.put(key, value);
+				}
+
+				System.out.println(Arrays.toString(cmdBits));
+				System.out.println("actionCode : " + actionCode);
+				System.out.println("params : " + params);
+				// parsing
+
+				wiseSayingController.remove();
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+			}
+		}
+	}
+}
--- src/com/ws/system/controller/SystemController.java
+++ src/com/ws/system/controller/SystemController.java
@@ -2,4 +2,9 @@ package com.ws.system.controller;
 
 public class SystemController {
 
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
